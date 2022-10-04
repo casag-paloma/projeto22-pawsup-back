2c4034053e15a2f712e2cb79dc24f55d13e8a168
Paloma <casagrande.paloma@gmail.com>
2022-10-04 14:02:08 -0300

    [feat] (post /signin) adds encryption to user password

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,13 +1,16 @@
 import * as userRepository from "../repositories/userRepository";
 import { IUserType } from "../types/userType";
 import { conflictError } from "../utils/errorUtil";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export async function createUser(userData: IUserType) {
     
     const user = await userRepository.getUserByEmail(userData.email);
-    if(user){
-        throw conflictError('this user is already cadastred');
-    }
+    if(user) throw conflictError('this user is already cadastred')
 
+    const encrypedPassword = bcrypt.hashSync(userData.password, 10);
+    userData.password = encrypedPassword;
+    console.log(userData);
     await userRepository.create(userData);
 }
